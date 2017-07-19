b294082d5e7c76f039596cd737faf84d208a0819
Xciel <ajj.bos89@gmail.com>
2017-07-19 22:19:31 +0200

    Second commit

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,6 +17,83 @@ window.Vue = require('vue');
 
 Vue.component('example', require('./components/Example.vue'));
 
+
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+
+    data: {
+        DisplayTasks: true,
+        deletedTasks: 0,
+        tasks: [],
+
+
+
+    },
+    mounted () {
+        this.fetch()
+    },
+
+
+
+
+    methods: {
+
+        AddTask: function(e) {
+            e.preventDefault();
+            axios.post('api/tasks', {
+                name: this.tasks.name,
+                done: false
+            }).then(response=> console.log(response));
+            this.tasks.push({
+                name: this.tasks.name,
+                done: false,
+            })
+        },
+
+        deleteTask: function (task) {
+
+            axios.delete('api/tasks/', {
+                name:this.tasks.name,
+            })
+            this.tasks.splice(this.tasks.indexOf(task), 1)
+            this.deletedTasks++;
+        },
+        fetch: function () {
+
+            axios.get('api/tasks/',).then
+            (response => {
+                console.log(response))
+                this.tasks.push(response.data)
+                }
+
+
+
+
+
+
+
+
+
+    ,
+    computed: {
+        checkMarkedTasks: function() {
+            let count = 0;
+            for (let i = 0; i < this.tasks.length, ++i;){
+                if (this.tasks[i].done === true) {
+                    count++;
+                }
+            }
+            return count;
+        },
+
+        ProgressieTasksCompleted: function () {
+            if (this.tasks.length == 0) {
+                return 0;
+            }else {
+                return (this.checkMarkedTasks / this.tasks.length) * 100;
+            }
+        }
+
+
+    }
 });
